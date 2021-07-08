a01c00fafd02f54698f92eeeb13340a5780b432b
Jordan Falcon <jordanfalcon@Jordans-MacBook-Pro.local>
2021-07-07 20:47:34 -0500

    Updated read me and gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://jordanfalcon.dev",
     title: "jordan falcon",
   },
   plugins: ["gatsby-plugin-gatsby-cloud"],
