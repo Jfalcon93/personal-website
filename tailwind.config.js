3dc5576d31d68b3ab6ec5fbe521008471b58cf30
Jordan Falcon <jordanfalcon@Jordans-MacBook-Pro.local>
2021-07-07 20:28:39 -0500

    Adding new files that weren't added in first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        "10xl": ["9rem", "1"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
